bbb6c78a51e564cde356cb7efb1ee1427d569dc0
lofthemof <joshuajiananli@gmail.com>
2025-03-12 16:09:01 -0500

    Authentication Component + Reorganization
    Used React Context to manage the state of a user context for user authentication. Also moved the routes folder from the components into the src folder.

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,37 @@
+import { Fragment, useContext } from "react";
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { UserContext } from "../../contexts/user.context";
+import "./navigation.styles.scss";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
+
+const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <Logo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              SIGN IN
+            </Link>
+          )}
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
